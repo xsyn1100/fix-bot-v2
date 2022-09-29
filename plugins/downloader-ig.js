e1abc4cbf2a4174be66016cbda3f3efeed1c5f79
Doni Ramadhan <98083245+xsyn1100@users.noreply.github.com>
2022-09-29 17:51:20 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,12 +1,14 @@
 import instagramGetUrl from 'instagram-url-direct'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `Contoh: ${usedPrefix}${command} https://www.instagram.com/p/CH1A1c9J5pY/?utm_medium=copy_link`
-    const results = (await instagramGetUrl(args[0])).url_list[0]
+let text = `
+let text = `
+Contoh: .ig https://www.instagram.com/p/CH1A1c9J5pY/?utm_medium=copy_link
+`.trim()
+  m.reply(text)
 
-    conn.sendFile(m.chat, results, 'instagram.mp4', ``, m)
 }
+
 handler.help = ['ig'].map(v => v + ' <url>')
-handler.tags = ['igg']
+handler.tags = ['instagram']
 
 handler.command = /^(ig(dl)?)$/i
 
